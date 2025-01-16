0764cffd5255a61100d1ca1a99cdd8dafbdf1f49
JShamli <167690787+JShamli@users.noreply.github.com>
2025-01-17 00:21:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,3 +19,131 @@ resource "azurerm_resource_group" "rgtest" {
   name     = "RG-Test"
   location = "East US"
 }
+
+resource "azurerm_public_ip" "testpublicip" {
+  name                = "test-publicip"
+  resource_group_name = azurerm_resource_group.rgtest.name
+  location            = azurerm_resource_group.rgtest.location
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_security_group" "testnsg" {
+  name                = "nsg-test"
+  location            = azurerm_resource_group.rgtest.location
+  resource_group_name = azurerm_resource_group.rgtest.name
+  depends_on = [azurerm_network_security_group.testnsg]
+  security_rule {
+    name                       = "allow_public_rdp"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+    
+  }
+}
+resource "azurerm_virtual_network" "testvnet" {
+  name                = "vnet-test"
+  location            = azurerm_resource_group.rgtest.location
+  resource_group_name = azurerm_resource_group.rgtest.name
+  address_space       = ["10.10.10.0/24"]
+  depends_on = [azurerm_network_security_group.testnsg]
+ }
+
+resource "azurerm_subnet" "testsubnet" {
+  name                 = "sub-test"
+  resource_group_name  = azurerm_resource_group.rgtest.name
+  virtual_network_name = azurerm_virtual_network.testvnet.name
+  address_prefixes     = ["10.10.10.0/28"]
+  depends_on = [azurerm_virtual_network.testvnet]
+}
+
+resource "azurerm_subnet_network_security_group_association" "testsubnet" {
+  subnet_id                 = azurerm_subnet.testsubnet.id
+  network_security_group_id = azurerm_network_security_group.testnsg.id
+  depends_on = [azurerm_subnet.testsubnet]
+}
+
+resource "azurerm_network_interface" "testnic" {
+  name                = "nic-testsrv"
+  location            = azurerm_resource_group.rgtest.location
+  resource_group_name = azurerm_resource_group.rgtest.name
+  depends_on = [azurerm_subnet_network_security_group_association.testsubnet]
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.testsubnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.testpublicip.id
+   }
+}
+
+resource "azurerm_windows_virtual_machine" "rgtest" {
+  name                = "example-machine"
+  resource_group_name = azurerm_resource_group.rgtest.name
+  location            = azurerm_resource_group.rgtest.location
+  size                = "Standard_B2s"
+  admin_username      = "nimda"
+  admin_password      = "Password@123"
+  network_interface_ids = [
+    azurerm_network_interface.testnic.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+}
+
+
+
+resource "azurerm_network_interface" "testnic1" {
+  name                = "nic-testsrv1"
+  location            = azurerm_resource_group.rgtest.location
+  resource_group_name = azurerm_resource_group.rgtest.name
+  depends_on = [azurerm_subnet_network_security_group_association.testsubnet]
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.testsubnet.id
+    private_ip_address_allocation = "Dynamic"
+    }
+}
+
+resource "azurerm_windows_virtual_machine" "rgtest1" {
+  name                = "testvm"
+  resource_group_name = azurerm_resource_group.rgtest.name
+  location            = azurerm_resource_group.rgtest.location
+  size                = "Standard_B2s"
+  admin_username      = "nimda"
+  admin_password      = "Password@123"
+  network_interface_ids = [
+    azurerm_network_interface.testnic1.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+}
+
+
+
+#5yrGtdIEVxkHjhG5ePvRilSp9CpaOvwUVqKE53Zgi2flKuZkCxkpJQQJ99BAACAAAAAAAAAAAAASAZDOb878
